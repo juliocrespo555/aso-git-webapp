c6b84ce1589f57ab0752395f83ce6795eacc744a
juliocrespo555 <solmer@outlook.com>
2023-12-04 18:16:32 +0000

    Redis operando

--- app/main.py
+++ app/main.py
@@ -4,13 +4,21 @@ import os
 import redis
 
 APP = flask.Flask(__name__)
+version=os.environ.get('VERSION', 'Desconocida')
+REDISPORT=os.environ['REDISPORT']
+REDISHOST=os.environ['REDISHOST']
+IS_REDIS_AVAILABLE=os.environ['IS_REDIS_AVAILABLE']
+is_redis_available=True if IS_REDIS_AVAILABLE.upper()=="TRUE" else False
+if is_redis_available:
+        redis_client = redis.Redis(host=REDISHOST, port=REDISPORT, decode_responses=True)
 
 @APP.route('/')
 def index():
-
-    r = redis.Redis(host=REDISHOST, port=REDISPORT, decode_responses=True)
-
-    num_visitas = r.incr("contrador_visitas")
+    global redis_client
+    if is_redis_available:
+        num_visitas = redis_client.incr("contrador_visitas")
+    else:
+        num_visitas = None
 
     hostname = socket.gethostname()
     userinfo = {
@@ -18,13 +26,12 @@ def index():
     }
     return flask.render_template(
             'index.html', 
-            user=userinfo, 
+            user=userinfo,
+            version=version,
             num_visitas=num_visitas, 
             hostname=hostname)
 
 if __name__ == '__main__':
     PORT=os.environ['PORT']
-    REDISPORT=os.environ['REDISPORT']
-    REDISHOST=os.environ['REDISHOST']
-    APP.debug = True
+    APP.debug = os.environ.get("DEBUG", True).upper() == "TRUE"
     APP.run(host='0.0.0.0', port=PORT)
